2dc99f5b368e5ad03e30a4e0941999fe73f5de20
jyallop <jyallop21@gmail.com>
2018-08-05 13:47:43 -0600

    Adds the preliminary word dictionary, edits the cabal file to include the dictionary, and adds some code to main to test that the file was added and adds a function that will take a word from a specified line number

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,21 @@ module Main where
 
 import Data.String.Strip
 import Game.Game
+import Paths_hangman
+import System.IO
 
 main :: IO ()
-main = print "Jonathon"
+main = do
+  fileName <- getDataFileName "words.txt"
+  file <- openFile fileName ReadMode
+  lines <- hGetContents file
+  let line = getNthWord 4 lines 
+  putStrLn line
+  print_game (new_game "rad")
+  print_game (make_guess (make_guess (new_game "welcome") 'w') 't')
+  print_game (Lost "hello")
+  print_game (Won "nice work" 4)
+  hClose file
+
+getNthWord :: Int -> String -> String
+getNthWord x = last . take x . lines
